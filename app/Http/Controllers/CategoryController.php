e3b4c331d50dc979fa6643931138d8d94e328fd6
junnatun <junsa02@gmail.com>
2025-06-21 15:19:58 +0700

    add edit categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -71,9 +72,20 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
         //
+        DB::transaction(function () use ($request, $category) {
+            $validated = $request->validated();
+            if ($request->hasFile('icon')) {
+                $iconPath = $request->file('icon')->store('icon', 'public');
+                $validated['icon'] = $iconPath;
+            }
+            $validated['slug'] = Str::slug($validated['name']);
+
+            $category->update($validated);
+        });
+        return redirect()->route('admin.categories.index');
     }
 
     /**
@@ -82,5 +94,14 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         //
+        DB::beginTransaction();
+        try {
+            $category->delete();
+            DB::commit();
+            return redirect()->route('admin.categories.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('admin.categories.index');
+        }
     }
 }
